88385df2b44d008f8c76927584b113ab9c83e7b9
Navod Abeywickrema Jayawardhane <njayawar@andrew.cmu.edu>
2024-11-26 23:02:20 -0500

    Added timing code

--- src/main.cpp
+++ src/main.cpp
@@ -102,34 +102,49 @@ std::unique_ptr<std::unordered_map<std::string, SignalType>> runPODEMRecursive(s
     // std:: cout << "Info: My current decision: " << myDecision.first << " | " << myDecision.second << std::endl;
 
 
-    std::array<std::unique_ptr<std::unordered_map<std::string, SignalType>>, 2> myPODEMResults;
-    #pragma omp parallel num_threads(2) 
-    {
-        int thread_num = omp_get_thread_num();
-        if (thread_num == 0) {
-            myDecision.second = SignalType::ZERO;
-        } else {
-            myDecision.second = SignalType::ONE;
-        }
-        Circuit myCircuit = *aCircuit;
-        myCircuit.setAndImplyCircuitInput(myDecision.first, myDecision.second);
-        myPODEMResults[thread_num] = runPODEMRecursive(myCircuit);
+    // START OMP implementation
+    // std::array<std::unique_ptr<std::unordered_map<std::string, SignalType>>, 2> myPODEMResults;
+    // #pragma omp parallel num_threads(2)
+    // {
+    //     int thread_num = omp_get_thread_num();
+    //     if (thread_num == 0) {
+    //         myDecision.second = SignalType::ZERO;
+    //     } else {
+    //         myDecision.second = SignalType::ONE;
+    //     }
+    //     Circuit myCircuit = *aCircuit;
+    //     myCircuit.setAndImplyCircuitInput(myDecision.first, myDecision.second);
+    //     myPODEMResults[thread_num] = runPODEMRecursive(myCircuit);
+    // }
+
+    // if (myPODEMResults[0] != NULL) {
+    //     return std::move(myPODEMResults[0]);
+    // } else if (myPODEMResults[1] != NULL) {
+    //     return std::move(myPODEMResults[1]);
+    // } else {
+    //     return NULL;
+    // }
+    // END OMP implementation
+
+    aCircuit->setAndImplyCircuitInput(myDecision.first, myDecision.second);
+    std::unique_ptr<std::unordered_map<std::string, SignalType>> myPODEMResult = runPODEMRecursive(aCircuit);
+    if(myPODEMResult != NULL){
+        return myPODEMResult;
     }
 
-    if (myPODEMResults[0] != NULL) {
-        return std::move(myPODEMResults[0]);
-    } else if (myPODEMResults[1] != NULL) {
-        return std::move(myPODEMResults[1]);
-    } else {
-        return NULL;
+    myDecision.second = (myDecision.second == SignalType::ONE) ? SignalType::ZERO : SignalType::ONE;
+    aCircuit->setAndImplyCircuitInput(myDecision.first, myDecision.second);
+    myPODEMResult = runPODEMRecursive(aCircuit);
+    if(myPODEMResult != NULL){
+        return myPODEMResult;
     }
+    aCircuit->setAndImplyCircuitInput(myDecision.first, SignalType::X);
+    return NULL;
 }
 
 
 std::unique_ptr<std::unordered_map<std::string, SignalType>> startPODEM(std::unique_ptr<Circuit>& aCircuit, std::pair<std::string, SignalType> anSSLFault){
     std::unique_ptr<std::unordered_map<std::string, SignalType>> myTestVector = std::make_unique<std::unordered_map<std::string, SignalType>>();
-
-    std::cout << "Info: Running PODEM to detect fault: " << anSSLFault.first << " | SA: " << (anSSLFault.second == SignalType::D ? '0' : '1') << std::endl;
 
     aCircuit->setCircuitFault(anSSLFault.first, anSSLFault.second);
     aCircuit->resetCircuit();
@@ -137,8 +152,14 @@ std::unique_ptr<std::unordered_map<std::string, SignalType>> startPODEM(std::uni
 }
 
 
-std::unique_ptr<std::vector<std::unordered_map<std::string, SignalType>>> runATPG(std::unique_ptr<Circuit>& aCircuit){
-    std::unique_ptr<std::vector<std::unordered_map<std::string, SignalType>>> myTestVectors = std::make_unique<std::vector<std::unordered_map<std::string, SignalType>>>();
+std::unique_ptr<std::vector<std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>>> runATPG(std::unique_ptr<Circuit>& aCircuit){
+
+    double myTotalComputationTime = 0.0;
+
+    // Unique_ptr to vector of results for each SSL fault ATPG
+    // Each result entry consists of: | SSL fault (pair of string and SignalType) | Computation type (double) | Generated Test Vector (Unordered map of input signal names and values) - return empty map if SSL fault undetectable |
+    std::unique_ptr<std::vector<std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>>> myATPGData = std::make_unique<std::vector<std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>>>();
+
     std::unique_ptr<std::vector<std::pair<std::string, SignalType>>> mySSLFaults = std::make_unique<std::vector<std::pair<std::string, SignalType>>>();
 
     std::unique_ptr<std::vector<std::string>> myTest = std::make_unique<std::vector<std::string>>();
@@ -152,9 +173,16 @@ std::unique_ptr<std::vector<std::unordered_map<std::string, SignalType>>> runATP
     while (!mySSLFaults->empty()){
         std::cout << "\nProgress: " << (myNumFaults - mySSLFaults->size()) << " / " << myNumFaults << " faults complete" << std::endl;
         std::pair<std::string, SignalType> myTargetSSLFault = mySSLFaults->back();
+
+        std::cout << "Info: Running PODEM to detect fault: " << myTargetSSLFault.first << " | SA: " << (myTargetSSLFault.second == SignalType::D ? '0' : '1') << std::endl;
+
+        const auto mySingleSSLATPGStartTime = std::chrono::steady_clock::now();
         std::unique_ptr<std::unordered_map<std::string, SignalType>> myTestVector = startPODEM(aCircuit, myTargetSSLFault);
+        const auto mySingleSSLATPGTime = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - mySingleSSLATPGStartTime).count();
+        myTotalComputationTime += mySingleSSLATPGTime;
+
         if (myTestVector != NULL){
-            myTestVectors->push_back(*myTestVector);
+            myATPGData->push_back(std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>(myTargetSSLFault, mySingleSSLATPGTime, *myTestVector));
 
             std::cout << "\n--- Found test vector for signal " << myTargetSSLFault.first << " | SA: " << (myTargetSSLFault.second == SignalType::D ? '0' : '1') << " ---" << std::endl;
             for (auto& [myTestVectorInputSignal, myTestVectorInputValue] : *myTestVector){
@@ -162,12 +190,17 @@ std::unique_ptr<std::vector<std::unordered_map<std::string, SignalType>>> runATP
             }
 
         } else {
+            myATPGData->push_back(std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>(myTargetSSLFault, mySingleSSLATPGTime, std::unordered_map<std::string, SignalType>()));
             std::cout << "Info: Unable to generate test vector for fault: " << myTargetSSLFault.first << " | SA: " << (myTargetSSLFault.second == SignalType::D ? '0' : '1') << std::endl;
         }
+
+        std::cout << "Computation time for single SSL fault ATPG (sec): " << std::fixed << std::setprecision(10) << mySingleSSLATPGTime << '\n';
         mySSLFaults->pop_back();
     }
 
-    return myTestVectors;
+    std::cout << "\nTotal ATPG Computation Time (sec): " << std::fixed << std::setprecision(10) << myTotalComputationTime << '\n';
+
+    return myATPGData;
 }
 
 
@@ -180,9 +213,13 @@ int main(int argc, char** argv) {
 
     std::unique_ptr<Circuit> myCircuit = std::make_unique<Circuit>(argv[1]);
 
-    std::unique_ptr<std::vector<std::unordered_map<std::string, SignalType>>> myTestVectors;
+    std::unique_ptr<std::vector<std::tuple<std::pair<std::string, SignalType>, double, std::unordered_map<std::string, SignalType>>>> myATPGData;
 
-    myTestVectors = runATPG(myCircuit);
+    myATPGData = runATPG(myCircuit);
+
+    for (auto& mySSLTestResult : *myATPGData) {
+        std::cout << std::get<0>(mySSLTestResult).first << "," << (std::get<0>(mySSLTestResult).second == SignalType::D ? '0' : '1') << "," << std::get<1>(mySSLTestResult) << "," << (!std::get<2>(mySSLTestResult).empty()) << std::endl;
+    }
 
     return 0;
 }
